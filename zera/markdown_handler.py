bd75d53eab63cfdfe595a6145e0cdaec06e4992e
Furkan Önder <furkantahaonder@gmail.com>
2019-09-18 20:29:05 +0300

    add utf8 decorater

--- zera/markdown_handler.py
+++ zera/markdown_handler.py
@@ -8,12 +8,20 @@ class MarkdownHandler(RequestHandler):
         super().__init__()
         self.content_type = "text/html"
 
+    def utf8_decorate(func):
+        def func_wrapper(content):
+            return f'<meta charset="UTF-8">{func(content)}'
+
+        return func_wrapper
+
+    @utf8_decorate
     def get_contents(self):
         return markdown2.markdown(self.contents.read())
 
     def find_template(self, url_template):
+        file = str("templates/" + url_template)
         try:
-            self.template_file = open(f"templates/" + url_template)
+            self.template_file = open(f"{file}")
             self.contents = self.template_file
             self.set_status(200)
             return True
