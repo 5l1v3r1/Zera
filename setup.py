15bb29b581365c45e6fa6659d95f5207389d5f86
Furkan <furkantahaonder@gmail.com>
2019-09-22 00:00:28 +0300

    add requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from pathlib import Path
-
 from setuptools import setup
+
+CURRENT_DIR = Path(__file__).parent
+
+def get_long_description():
+    readme_md = CURRENT_DIR / "README.md"
+    with open(readme_md, encoding="utf8") as ld_file:
+        return ld_file.read()
 
 setup(
     name="zera",
@@ -9,9 +15,12 @@ setup(
     author="Furkan Onder",
     author_email="furkantahaonder@gmail.com",
     description="static website generator",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     license="MIT",
     keywords="static web generator html markdown",
     url="https://github.com/furkanonder/Zera",
     include_package_data=True,
     entry_points={"console_scripts": ["zera = zera.zera:main"]},
+    install_requires=["markdown2", "click"],
 )
